91d1c3c3e014ff0cb5f04891f72757650799173d
Professor Andre Silva <andre@fiar.local>
2019-06-06 20:32:55 -0300

    Scripts - Banco de dados

---
+++ Data/scripts/tb_cidade.sql
@@ -0,0 +1,32 @@
+﻿/*    ==Parâmetros de Script==
+
+    Versão do Servidor de Origem : SQL Server 2016 (13.0.4224)
+    Edição do Mecanismo de Banco de Dados de Origem : Microsoft SQL Server Enterprise Edition
+    Tipo do Mecanismo de Banco de Dados de Origem : SQL Server Autônomo
+
+    Versão do Servidor de Destino : SQL Server 2016
+    Edição de Mecanismo de Banco de Dados de Destino : Microsoft SQL Server Enterprise Edition
+    Tipo de Mecanismo de Banco de Dados de Destino : SQL Server Autônomo
+*/
+
+USE [dbcadastro]
+GO
+
+/****** Object:  Table [dbo].[tb_cidade]    Script Date: 06/06/2019 20:30:28 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tb_cidade](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[descricao] [varchar](50) NULL,
+ CONSTRAINT [PK_tb_cidade] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ Data/scripts/tb_pessoa.sql
@@ -0,0 +1,41 @@
+﻿/*    ==Parâmetros de Script==
+
+    Versão do Servidor de Origem : SQL Server 2016 (13.0.4224)
+    Edição do Mecanismo de Banco de Dados de Origem : Microsoft SQL Server Enterprise Edition
+    Tipo do Mecanismo de Banco de Dados de Origem : SQL Server Autônomo
+
+    Versão do Servidor de Destino : SQL Server 2016
+    Edição de Mecanismo de Banco de Dados de Destino : Microsoft SQL Server Enterprise Edition
+    Tipo de Mecanismo de Banco de Dados de Destino : SQL Server Autônomo
+*/
+
+USE [dbcadastro]
+GO
+
+/****** Object:  Table [dbo].[tb_pessoa]    Script Date: 06/06/2019 20:30:35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tb_pessoa](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nome] [varchar](50) NULL,
+	[telefone] [varchar](50) NULL,
+	[idcidade] [int] NULL,
+ CONSTRAINT [PK_tb_pessoa] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tb_pessoa]  WITH CHECK ADD  CONSTRAINT [FK_tb_pessoa_tb_cidade] FOREIGN KEY([idcidade])
+REFERENCES [dbo].[tb_cidade] ([id])
+GO
+
+ALTER TABLE [dbo].[tb_pessoa] CHECK CONSTRAINT [FK_tb_pessoa_tb_cidade]
+GO
+
+
